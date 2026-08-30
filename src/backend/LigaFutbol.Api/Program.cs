1adb240ab9a67a72e084602772aacae8970a19d1
Marcelo Casanovas <maxcasanovas@gmail.com>
2026-08-30 01:58:10 -0400

    +extension snakeCase parameter, para formatear Pascal Case a Snake Case en endpoints

--- src/backend/LigaFutbol.Api/Program.cs
+++ src/backend/LigaFutbol.Api/Program.cs
@@ -1,8 +1,10 @@
 using LigaFutbol.Api.Data;
+using LigaFutbol.Api.Extensions;
 using LigaFutbol.Api.Repositories;
 using LigaFutbol.Api.Repositories.Interfaces;
 using LigaFutbol.Api.Services;
 using LigaFutbol.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
@@ -10,7 +12,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    // Convierte los tokens [controller]/[action] a snake_case (ej: MiRecurso -> mi_recurso)
+    options.Conventions.Add(new RouteTokenTransformerConvention(new SnakeCaseParameterTransformer()));
+});
+
+builder.Services.AddRouting(options =>
+{
+    // Fuerza minúsculas en toda la URL generada (segmentos y query string)
+    options.LowercaseUrls = true;
+    options.LowercaseQueryStrings = true;
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
